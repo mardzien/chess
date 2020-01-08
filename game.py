d656190b51dd968857c61d38d1b222dcb772dcc6
mardzien <mardzien@gmail.com>
2020-01-08 21:15:01 +0100

    Pieces initialization

--- game.py
+++ game.py
@@ -23,6 +23,86 @@ class Chessboard(object):
         pp.pprint(self.fields)
 
 
+class Piece(object):
+
+    def __init__(self, color):
+        self.color = color
+
+
+class Pawn(Piece):
+
+    def __init__(self, color):
+        super().__init__(color)
+        if color == "WHITE":
+            self.graphic_representation = "♙"
+        elif color == "BLACK":
+            self.graphic_representation = "♟"
+        else:
+            raise Exception("Invalid piece color.")
+
+
+class Rook(Piece):
+
+    def __init__(self, color):
+        super().__init__(color)
+        if color == "WHITE":
+            self.graphic_representation = "♖"
+        elif color == "BLACK":
+            self.graphic_representation = "♜"
+        else:
+            raise Exception("Invalid piece color.")
+
+
+class Bishop(Piece):
+
+    def __init__(self, color):
+        super().__init__(color)
+        if color == "WHITE":
+            self.graphic_representation = "♗"
+        elif color == "BLACK":
+            self.graphic_representation = "♝"
+        else:
+            raise Exception("Invalid piece color.")
+
+
+class Knight(Piece):
+
+    def __init__(self, color):
+        super().__init__(color)
+        if color == "WHITE":
+            self.graphic_representation = "♘"
+        elif color == "BLACK":
+            self.graphic_representation = "♞"
+        else:
+            raise Exception("Invalid piece color.")
+
+
+class King(Piece):
+
+    def __init__(self, color):
+        super().__init__(color)
+        if color == "WHITE":
+            self.graphic_representation = "♔"
+        elif color == "BLACK":
+            self.graphic_representation = "♚"
+        else:
+            raise Exception("Invalid piece color.")
+
+
+class Queen(Piece):
+
+    def __init__(self, color):
+        super().__init__(color)
+        if color == "WHITE":
+            self.graphic_representation = "♕"
+        elif color == "BLACK":
+            self.graphic_representation = "♛"
+        else:
+            raise Exception("Invalid piece color.")
+
+
 if __name__ == "__main__":
     board = Chessboard()
     board.display()
+
+
