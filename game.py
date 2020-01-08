96d3f5f54f1a2cda3d33c988ea07c3020998e302
mardzien <mardzien@gmail.com>
2020-01-08 21:46:47 +0100

    First displey test

--- game.py
+++ game.py
@@ -4,6 +4,9 @@ Chess game
 from collections import namedtuple
 import pprint
 
+WHITE = "WHITE"
+BLACK = "BLACK"
+
 pp = pprint.PrettyPrinter(indent=4)
 
 Field = namedtuple('Field', ['x', 'y'])
@@ -27,15 +30,22 @@ class Piece(object):
 
     def __init__(self, color):
         self.color = color
+        self.graphic_representation = None
+
+    def display(self):
+        if self.graphic_representation is None:
+            raise Exception("Invalid graphic representation.")
+        else:
+            print(self.graphic_representation)
 
 
 class Pawn(Piece):
 
     def __init__(self, color):
         super().__init__(color)
-        if color == "WHITE":
+        if color == BLACK:
             self.graphic_representation = "♙"
-        elif color == "BLACK":
+        elif color == WHITE:
             self.graphic_representation = "♟"
         else:
             raise Exception("Invalid piece color.")
@@ -45,9 +55,9 @@ class Rook(Piece):
 
     def __init__(self, color):
         super().__init__(color)
-        if color == "WHITE":
+        if color == BLACK:
             self.graphic_representation = "♖"
-        elif color == "BLACK":
+        elif color == WHITE:
             self.graphic_representation = "♜"
         else:
             raise Exception("Invalid piece color.")
@@ -57,9 +67,9 @@ class Bishop(Piece):
 
     def __init__(self, color):
         super().__init__(color)
-        if color == "WHITE":
+        if color == BLACK:
             self.graphic_representation = "♗"
-        elif color == "BLACK":
+        elif color == WHITE:
             self.graphic_representation = "♝"
         else:
             raise Exception("Invalid piece color.")
@@ -69,9 +79,9 @@ class Knight(Piece):
 
     def __init__(self, color):
         super().__init__(color)
-        if color == "WHITE":
+        if color == BLACK:
             self.graphic_representation = "♘"
-        elif color == "BLACK":
+        elif color == WHITE:
             self.graphic_representation = "♞"
         else:
             raise Exception("Invalid piece color.")
@@ -81,9 +91,9 @@ class King(Piece):
 
     def __init__(self, color):
         super().__init__(color)
-        if color == "WHITE":
+        if color == BLACK:
             self.graphic_representation = "♔"
-        elif color == "BLACK":
+        elif color == WHITE:
             self.graphic_representation = "♚"
         else:
             raise Exception("Invalid piece color.")
@@ -93,16 +103,27 @@ class Queen(Piece):
 
     def __init__(self, color):
         super().__init__(color)
-        if color == "WHITE":
+        if color == BLACK:
             self.graphic_representation = "♕"
-        elif color == "BLACK":
+        elif color == WHITE:
             self.graphic_representation = "♛"
         else:
             raise Exception("Invalid piece color.")
 
 
 if __name__ == "__main__":
+    print("Chessboard: ")
     board = Chessboard()
     board.display()
 
+    piece_types = [Pawn, Knight, Bishop, Rook, Queen, King]
+    white_pieces = [t(color=WHITE) for t in piece_types]
+    black_pieces = [t(color=BLACK) for t in piece_types]
 
+    print("White pieces:")
+    for piece in white_pieces:
+        piece.display()
+
+    print("Black pieces:")
+    for piece in black_pieces:
+        piece.display()
