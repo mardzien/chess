9b543776ba7bc4d7a479d6bb98bdcb8248fe97d4
mardzien <mardzien@gmail.com>
2020-01-08 20:37:50 +0100

    Chessboard initialization

--- game.py
+++ game.py
@@ -1,7 +1,28 @@
 """
 Chess game
 """
+from collections import namedtuple
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+Field = namedtuple('Field', ['x', 'y'])
 
 
 class Chessboard(object):
-    pass
+
+    def __init__(self):
+        size = 8
+        self.fields = [[self.initialize_field(i, j) for j in range(size)] for i in range(size)]
+
+    @staticmethod
+    def initialize_field(x, y):
+        return Field(x, y)
+
+    def display(self):
+        pp.pprint(self.fields)
+
+
+if __name__ == "__main__":
+    board = Chessboard()
+    board.display()
